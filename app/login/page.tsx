bf06f75c2f63bf729107a1d3c9d320bf28d4f7aa
Lee Yuntae <akor1@naver.com>
2024-07-17 00:18:16 +0900

    ✨ Server actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,13 @@
-"use client";
-
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
-  const onClick = async () => {
-    const response = await fetch("/www/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "kim",
-        password: "1234",
-      }),
-    });
-    console.log(await response.json());
-  };
+  async function handleForm(formData: FormData) {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("i run in the server!");
+  }
 
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
@@ -22,18 +15,23 @@ export default function LogIn() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
           errors={[]}
         />
-      </form>
-      <span onClick={onClick}>
         <FormButton loading={false} text="Log in" />
-      </span>
+      </form>
       <SocialLogin />
     </div>
   );
