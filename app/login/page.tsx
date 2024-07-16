fb7a7a48af9d4901dcf0b3f695aa699beebe0f73
Lee Yuntae <akor1@naver.com>
2024-07-17 00:21:21 +0900

    🎨 UseFormStatus

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,8 +5,8 @@ import SocialLogin from "@/components/social-login";
 export default function LogIn() {
   async function handleForm(formData: FormData) {
     "use server";
-    console.log(formData.get("email"), formData.get("password"));
-    console.log("i run in the server!");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("logged in");
   }
 
   return (
@@ -30,7 +30,7 @@ export default function LogIn() {
           required
           errors={[]}
         />
-        <FormButton loading={false} text="Log in" />
+        <FormButton text="Log in" />
       </form>
       <SocialLogin />
     </div>
