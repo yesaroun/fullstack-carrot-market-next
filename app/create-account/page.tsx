4b106edaa289393f78b58715d0d3be17786d3486
Lee Yuntae <akor1@naver.com>
2024-07-17 01:12:31 +0900

    ✨ Zod 적용

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,22 +1,49 @@
-import Link from "next/link";
-import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
+"use client";
+
 import FormInput from "@/components/form-input";
 import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "@/app/create-account/actions";
 
 export default function CreateAccount() {
+  const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-2xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="Username" required errors={[]} />
-        <FormInput type="email" placeholder="Email" required errors={[]} />
-        <FormInput type="password" placeholder="Password" required errors={[]} />
-        <FormInput type="password" placeholder="Confirm Password" required errors={[]} />
-        <FormButton loading={false} text="Create account" />
+      <form action={dispatch} className="flex flex-col gap-3">
+        <FormInput
+          name="username"
+          type="text"
+          placeholder="Username"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="pemail"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="confirm_password"
+          type="password"
+          placeholder="Confirm Password"
+          required
+          errors={[]}
+        />
+        <FormButton text="Create account" />
       </form>
       <SocialLogin />
     </div>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -21,14 +21,12 @@ export default function LogIn() {
           type="email"
           placeholder="Email"
           required
-          errors={[]}
         />
         <FormInput
           name="password"
           type="password"
           placeholder="Password"
           required
-          errors={state?.errors ?? []}
         />
         <FormButton text="Log in" />
       </form>
